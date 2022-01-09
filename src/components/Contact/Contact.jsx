e45419b3fa706ba09abf8dbcbfcfb16194e3f009
Kishan <iamkishanchandravanshi@gmail.com>
2022-01-09 16:43:05 +0530

    added other sections and smooth scroll effect

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+export default function Contact() {
+  return (
+    <div id="contact">
+      <h2>{"<Contact />"}</h2>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+      atque voluptate ab deserunt totam laudantium in qui recusandae, hic alias
+      dolore molestiae doloremque animi provident quibusdam non ea aliquam
+      mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero
+      sequi doloribus laudantium repellat, eveniet non reiciendis fugiat velit,
+      corporis amet ipsum necessitatibus. Incidunt maiores fugiat ex voluptates
+      deserunt quidem quae. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Pariatur, nulla blanditiis, veritatis possimus quidem excepturi
+      error eius perferendis facere laborum est harum dicta laboriosam
+      recusandae cupiditate. Quod repellendus cumque Lorem, ipsum dolor sit amet
+      consectetur adipisicing elit. Quis impedit maiores nam molestias
+      reprehenderit eaque alias libero commodi facilis vitae, adipisci soluta
+      accusamus error iure, tempora nobis dolor, assumenda officiis? possimus
+      quidem excepturi error eius perferendis facere laborum est harum dicta
+      laboriosam recusandae cupiditate. Quod repellendus cumque Lorem, ipsum
+      dolor sit amet consectetur adipisicing elit. Quis impedit maiores nam
+      molestias reprehenderit eaque alias libero commodi facilis vitae, adipisci
+      soluta accusamus error iure, tempora nobis dolor, assumenda officiis?
+      possimus quidem excepturi error eius perferendis facere laborum est harum
+      dicta laboriosam recusandae cupiditate. Quod repellendus cumque Lorem,
+      ipsum dolor sit amet consectetur adipisicing elit. Quis impedit maiores
+      nam molestias reprehenderit eaque alias libero commodi facilis vitae,
+      adipisci soluta accusamus error iure, tempora nobis dolor, assumenda
+      officiis?
+    </div>
+  );
+}
---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,43 @@
+export default function Work() {
+  return (
+    <div id="work">
+      <h2>{"<Work />"}</h2>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quia
+      provident, ducimus quas commodi rem, vero dolores recusandae consequatur,
+      voluptates qui autem temporibus fugiat aliquam quibusdam necessitatibus
+      reprehenderit amet. Deleniti. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Voluptatibus atque voluptate ab deserunt totam
+      laudantium in qui recusandae, hic alias dolore molestiae doloremque animi
+      provident quibusdam non ea aliquam mollitia! Lorem ipsum dolor sit amet,
+      consectetur adipisicing elit. Vero sequi doloribus laudantium repellat,
+      eveniet non reiciendis fugiat velit, corporis amet ipsum necessitatibus.
+      Incidunt maiores fugiat ex voluptates deserunt quidem quae. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Pariatur, nulla blanditiis,
+      veritatis possimus quidem excepturi error eius perferendis facere laborum
+      est harum dicta laboriosam recusandae cupiditate. Quod repellendus cumque
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis impedit
+      maiores nam molestias reprehenderit eaque alias libero commodi facilis
+      vitae, adipisci soluta accusamus error iure, tempora nobis dolor,
+      assumenda officiis? possimus quidem excepturi error eius perferendis
+      facere laborum est harum dicta laboriosam recusandae cupiditate. Quod
+      repellendus cumque Lorem, ipsum dolor sit amet consectetur adipisicing
+      elit. Quis impedit maiores nam molestias reprehenderit eaque alias libero
+      commodi facilis vitae, adipisci soluta accusamus error iure, tempora nobis
+      dolor, assumenda officiis? possimus quidem excepturi error eius
+      perferendis facere laborum est harum dicta laboriosam recusandae
+      cupiditate. Quod repellendus cumque Lorem, ipsum dolor sit amet
+      consectetur adipisicing elit. Quis impedit maiores nam molestias
+      reprehenderit eaque alias libero commodi facilis vitae, adipisci soluta
+      accusamus error iure, tempora nobis dolor, assumenda officiis? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Voluptatibus atque voluptate
+      ab deserunt totam laudantium in qui recusandae, hic alias dolore molestiae
+      doloremque animi provident quibusdam non ea aliquam mollitia! Lorem ipsum
+      dolor sit amet, consectetur adipisicing elit. Vero sequi doloribus
+      laudantium repellat, eveniet non reiciendis fugiat velit, corporis amet
+      ipsum necessitatibus. Incidunt maiores fugiat ex voluptates deserunt
+      quidem quae. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Pariatur, nulla blanditiis, veritatis possimus quidem excepturi error eius
+      perferendis facere laborum est harum dicta laboriosam recusandae
+    </div>
+  );
+}
